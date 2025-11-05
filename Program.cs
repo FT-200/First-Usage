beb6f91ceea5e186ca02c42a8238f8487fe390ba
user <user@MYROGPC>
2025-11-05 22:51:49 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+namespace KSAtest__delete_
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+            
+            
+            app.MapGet("/", () => "Baha, Do some work, from now on, you're going to Die right now");
+            
+            app.Run();
+        }
+
+        
+
+        }
+    }
